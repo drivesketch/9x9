efc9a458291eed20340779d0765fe78e6ea2d000
so <hiroaki.so@smarthr.co.jp>
2023-03-04 17:20:07 +0900

    ファイル追加

---
+++ script.js
@@ -0,0 +1,31 @@
+let factor1, factor2, answer;
+const spanFactor1 = document.getElementById('factor1');
+const spanFactor2 = document.getElementById('factor2');
+const spanAnswer = document.getElementById('answer');
+const buttonNewQuestion = document.getElementById('buttonNewQuestion');
+const buttonShowAnswer = document.getElementById('buttonShowAnswer');
+
+const newQuestion = () => {
+    factor1 = Math.floor(Math.random() * 8) + 2 ;
+    factor2 = Math.floor(Math.random() * 8) + 2 ;
+    answer = factor1 * factor2;
+    spanFactor1.textContent = factor1;
+    spanFactor2.textContent = factor2;
+    spanAnswer.textContent = '???';
+    buttonNewQuestion.classList.toggle('hidden');
+    buttonShowAnswer.classList.toggle('hidden');
+}
+
+const showAnswer = () => {
+    spanAnswer.textContent = answer;
+    buttonNewQuestion.classList.toggle('hidden');
+    buttonShowAnswer.classList.toggle('hidden');
+}
+
+window.onload = newQuestion();
+
+// buttonShowAnswer.onclick = showAnswer();
+// buttonNewQuestion.onclick = newNumbers();
+
+
+// console.log(`${factor1} x ${factor2} = ${answer(factor1, factor2)}`);
